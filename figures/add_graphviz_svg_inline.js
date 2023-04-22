a3f6aae0b2caca51cb0aba20ae400614b91cfc40
Max Brown <max.brown@ed.ac.uk>
2023-04-22 10:00:33 +0100

    More interaction on the crossing diagrams.

--- figures/add_graphviz_svg_inline.js
+++ figures/add_graphviz_svg_inline.js
@@ -1,5 +1,3 @@
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
-
 // these are the files and their class names
 // I could have programmatically generated the classes
 // but oh well!
@@ -13,6 +11,7 @@ const files = {
     "diploid_tetraploid_hybridisation3",
   "./crosses/diploid_tetraploid_hybridisation4.svg":
     "diploid_tetraploid_hybridisation4",
+  // TODO: these last graphs
   // "./crosses/backcross_triploid1.svg": "",
   // "./crosses/backcross_triploid2.svg": "",
 };
@@ -35,32 +34,70 @@ for (const key in files) {
       const endosperm_elements =
         document.getElementsByClassName("endosperm_outcome");
 
-      function changeStyle(elements, color) {
+      function changeStyle(elements, arrow_color, ellipse_color) {
+        let originalArrowColor, originalEllipseColor;
         for (var i = 0; i < elements.length; i++) {
           // because the nodes are in a <g> element.
-          elements[i].querySelector(":nth-child(2)").style.fill = color;
+          let el = elements[i].querySelector(":nth-child(2)");
+          if (el.nodeName == "path") {
+            originalArrowColor =
+              elements[i].querySelector(":nth-child(2)").style.stroke;
+            // path element
+            elements[i].querySelector(":nth-child(2)").style.stroke =
+              arrow_color;
+            // arrowhead
+            elements[i].querySelector(":nth-child(3)").style.fill = arrow_color;
+            elements[i].querySelector(":nth-child(3)").style.stroke =
+              arrow_color;
+          } else {
+            originalEllipseColor =
+              elements[i].querySelector(":nth-child(2)").style.fill;
+            elements[i].querySelector(":nth-child(2)").style.fill =
+              ellipse_color;
+          }
+        }
+        return [originalArrowColor, originalEllipseColor];
+      }
+
+      //
+      function createMouseEvents(hover_elements, all_elements) {
+        // get the length of the elements
+        let n = hover_elements.length;
+        // now iterate over the elements
+        for (let i = 0; i < n; i++) {
+          // find out what the current graph is by looking at the class name
+          let curr_el = hover_elements[i];
+          // [1] because [0] is always edge/node
+          let curr_el_name = curr_el.className.baseVal.split(" ")[1];
+          // filter the elements based on the current graph
+          let current_graph = Array.from(all_elements).filter((d) =>
+            d.className.baseVal.includes(curr_el_name)
+          );
+
+          // add the styling to the SVG
+          let prev_arrow_color, prev_ellipse_color;
+          hover_elements[i].onmouseover = function () {
+            [prev_arrow_color, prev_ellipse_color] = changeStyle(
+              current_graph,
+              "#616469",
+              "yellow"
+            );
+          };
+          hover_elements[i].onmouseout = function () {
+            changeStyle(current_graph, prev_arrow_color, prev_ellipse_color);
+          };
         }
       }
+
       // now just select the element which we will hover over
       let hybrid_outcome_hover_elements = document.getElementsByClassName(
         "hybrid_outcome onhover"
       );
-      let n = hybrid_outcome_hover_elements.length;
-
-      for (let i = 0; i < n; i++) {
-        let curr_el = hybrid_outcome_hover_elements[i];
-        let curr_el_name = curr_el.className.baseVal.split(" ")[1];
-        let current_graph = Array.from(hybrid_outcome_elements).filter((d) =>
-          d.className.baseVal.includes(curr_el_name)
-        );
-
-        hybrid_outcome_hover_elements[i].onmouseover = function () {
-          changeStyle(current_graph, "yellow");
-        };
-        hybrid_outcome_hover_elements[i].onmouseout = function () {
-          changeStyle(current_graph, "none");
-        };
-      }
+      let endosperm_hover_elements = document.getElementsByClassName(
+        "endosperm_outcome onhover"
+      );
+      createMouseEvents(hybrid_outcome_hover_elements, hybrid_outcome_elements);
+      createMouseEvents(endosperm_hover_elements, endosperm_elements);
     })
     .catch(console.error.bind(console));
 }
