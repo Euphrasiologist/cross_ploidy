3a805a247b4186ca4a7868ebe6b961d6cd1cdb2e
Max Brown <max.brown@ed.ac.uk>
2024-02-08 11:19:14 +0000

    Script now places figures in the correct place.

--- figures/crosses/generate_hybridisation_figures.bash
+++ figures/crosses/generate_hybridisation_figures.bash
@@ -1,7 +1,7 @@
 # generate all of the SVG's we want
 for GRAPH in ./*.dot; do
     NAME=$(basename $GRAPH .dot)
-    dot -Gfontname=Arial -Gdpi=400 -Nfontname=Arial -Efontname=Arial $GRAPH -Tpng:cairo > "./${NAME}.png"
-    dot -Gfontname=Arial -Nfontname=Arial -Efontname=Arial $GRAPH -Tsvg > "../figures/${NAME}.svg"
+    dot -Gfontname=Arial -Gdpi=400 -Nfontname=Arial -Efontname=Arial $GRAPH -Tpng:cairo > "../figures/${NAME}.png"
+    dot -Gfontname=Arial -Nfontname=Arial -Efontname=Arial $GRAPH -Tsvg > "./${NAME}.svg"
     echo "Layout for ${NAME} done."
 done
